30e4753f2aca64fd9fca6548bbd84cbbfe271d40
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-11-27 17:23:10 +0100

    made unit tests succeed again

--- lapis_tests/test_job.py
+++ lapis_tests/test_job.py
@@ -51,9 +51,13 @@ class TestJob(object):
             scheduling_duration=0,
             connection=Connection(),
         )
-        await drone.run()
         async with Scope() as scope:
+            scope.do(drone.run(), volatile=True)
             scope.do(drone.schedule_job(job=job))
+            await (
+                scheduler.statistics._available
+                == scheduler.statistics.resource_type(job_succeeded=1)
+            )
         assert 10 == time.now
         assert 0 == job.waiting_time
         assert job.successful
@@ -70,9 +74,13 @@ class TestJob(object):
             pool_resources={"cores": 1, "memory": 1},
             scheduling_duration=0,
         )
-        await drone.run()
         async with Scope() as scope:
+            scope.do(drone.run(), volatile=True)
             scope.do(drone.schedule_job(job=job))
+            await (
+                scheduler.statistics._available
+                == scheduler.statistics.resource_type(job_failed=1)
+            )
         assert 0 == time
         assert not job.successful
         assert 0 == job.waiting_time
@@ -93,10 +101,14 @@ class TestJob(object):
             pool_resources={"cores": 1, "memory": 1},
             scheduling_duration=0,
         )
-        await drone.run()
         async with Scope() as scope:
+            scope.do(drone.run(), volatile=True)
             scope.do(drone.schedule_job(job=job_one))
             scope.do(drone.schedule_job(job=job_two))
+            await (
+                scheduler.statistics._available
+                == scheduler.statistics.resource_type(job_succeeded=1, job_failed=1)
+            )
         assert 10 == time
         assert job_one.successful
         assert not job_two.successful
@@ -119,10 +131,14 @@ class TestJob(object):
             pool_resources={"cores": 2, "memory": 2},
             scheduling_duration=0,
         )
-        await drone.run()
         async with Scope() as scope:
+            scope.do(drone.run(), volatile=True)
             scope.do(drone.schedule_job(job=job_one))
             scope.do(drone.schedule_job(job=job_two))
+            await (
+                scheduler.statistics._available
+                == scheduler.statistics.resource_type(job_succeeded=2)
+            )
         assert 10 == time
         assert job_one.successful
         assert job_two.successful
