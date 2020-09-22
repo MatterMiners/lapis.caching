3015991ea4edc3dcc59ad5381eccf1d43fdc9ef6
tfesenbecker <t.fehsenbecker@gmx.de>
2020-09-22 13:20:09 +0200

    Added draft for job class documentation

--- lapis/job.py
+++ lapis/job.py
@@ -11,6 +11,56 @@ if TYPE_CHECKING:
 
 
 class Job(object):
+    """
+    Objects of this class represent jobs. The job is described from the batch
+    system's viewpoint by the following attributes:
+        :param resources: information about the resources requested by the job
+        :param used_resources: information about the resources used by the job
+        :param walltime: the job's runtime, in reality as well as in the simulation
+        :param requested_walltime: walltime requested by the job
+        :param cputime: the cumulated amount of time the used CPU(s) was (were) active
+        during the job's execution
+        :param queue_date: time when the job was submitted to the simulated job queue
+        _success: represents whether the job was run successfully
+
+    In addition, functionality allowing the simulation of data transfers is provided. In
+    this case, the job attributes have to be extended by information about the job's
+    input data. In this case the job's runtime is recalculated if the job processes
+    input data and is executed on resources with access to caches. In this case data
+    transfer and processing are assumed to be done in parallel. This is a valid
+    assumption if the input data are divided into blocks, transfered
+    throughout the job's runtime and if already transferred data blocks are processed
+    while other blocks are fetched. If the job's overall runtime is long and if the
+    data set was transferred in a large number of blocks, the job's runtime (walltime)
+    can be recalculated using max(calculation time, transfer time).
+
+        :param requested_inputfiles: information about the input files requested by a job
+        :param used_inputfiles: information about the input files actually read by a job
+        :param _total_input_data: data volume of used_inputfiles, amount of data
+        processed by the job
+        :param _original_walltime: the job's walltime as the simulation's input
+        states. Is stored for monitoring purposes as the job's walltime can be
+        altered
+        :param _calculation_time: the calculation time represents the time needed to
+        process the job's input data
+        :param calculation_efficiency: represents the efficiency of calculations
+        performed on the job's input data. Default = 1.0. Can be modified to take
+        programmatical insufficiencies into account. 
+        :param _transfer_time: the transfer time represents the time needed to
+        transfer the job's input data.
+
+    As the simulation of data transfers is used to simulate and study caching,
+    the following metadata are introduced and used for monitoring purposes.
+        :param _read_from_cache: true if job read data from cache
+        :param _cached_data: the amount of input data that is currently cached
+        :param failed_matches: number of times a match of this job to a resource was
+        rejected (see scheduler for details)
+        :param cache_probability: the averaged probability that all data are cached
+        (sum(filesize * hitrate = probability that file is cached) / sum(filesize))
+        :param expectation_cached_data: the expectation value for the amount of
+        cached data (sum(filesize * hitrate))
+    """
+
     __slots__ = (
         "resources",
         "used_resources",
@@ -46,21 +96,21 @@ class Job(object):
         queue_date: float = 0,
         name: Optional[str] = None,
         drone: "Optional[Drone]" = None,
-        calculation_efficiency: Optional[float] = None,
+        calculation_efficiency: Optional[float] = 1.0,
     ):
         """
-        Definition of a job that uses a specified amount of resources `used_resources`
-        over a given amount of time, `walltime`. A job is described by its user
-        via the parameter `resources`. This is a user prediction and is expected
-        to deviate from `used_resources`.
+        Initialization of a job
 
-        :param resources: Requested resources of the job
-        :param used_resources: Resource usage of the job
+        :param resources: Requested resources of the job, including walltime and
+        input data
+        :param used_resources: Resource usage of the job, including walltime and
+        input data
         :param in_queue_since: Time when job was inserted into the queue of the
                                simulation scheduler
         :param queue_date: Time when job was inserted into queue in real life
         :param name: Name of the job
         :param drone: Drone where the job is running on
+        :param calculation_efficiency:
         """
         self.resources = resources
         self.used_resources = used_resources
@@ -91,13 +141,23 @@ class Job(object):
         self._original_walltime = self.walltime
         self._calculation_time = 0
         self._transfer_time = 0
-        self.cputime = self.used_resources["cores"] * self.walltime
+
+        # TODO: this try-except is a fix for a unit test, check whether this makes
+        #  sense in all use cases
+        try:
+            self.cputime = self.used_resources["cores"] * self.walltime
+        except KeyError:
+            self.cputime = None
+
         try:
             self._total_input_data = sum(
                 [fileinfo["usedsize"] for fileinfo in self.used_inputfiles.values()]
             )
         except AttributeError:
             self._total_input_data = 0
+
+        # TODO: see unit test test_read_with_inputfiles -> decide whether making
+        #  information about hitrates obilgatory is actually necessary
         if self._total_input_data:
             self.expectation_cached_data = sum(
                 [
@@ -107,6 +167,7 @@ class Job(object):
             )
         else:
             self.expectation_cached_data = 0
+
         if self._total_input_data:
             self.cache_probability = sum(
                 [
@@ -116,6 +177,7 @@ class Job(object):
             ) / sum([file["usedsize"] for file in self.used_inputfiles.values()])
         else:
             self.cache_probability = 0
+
         self.failed_matches = 0
 
     @property
@@ -129,7 +191,7 @@ class Job(object):
     @property
     def waiting_time(self) -> float:
         """
-        The time the job spent in the simulators scheduling queue. `Inf` when
+        Determines the time the job spent in the simulated scheduling queue. `Inf` when
         the job is still waiting.
 
         :return: Time in queue
@@ -142,6 +204,13 @@ class Job(object):
         """
         Determines a jobs calculation time based on the jobs CPU time and a
         calculation efficiency representing inefficient programming.
+
+        If a job contains input files and the drone the job runs on has a defined remote
+        connection (throughput < Inf) the calculation time is given by job's CPU time
+        divided by a configurable `calculation_efficiency` that can be set != 1,  e.g. to
+        account for programmatical inefficiencies.
+
+        Else, the calculation time remains equal to the job's orginal `walltime`.
         :param calculation_efficiency:
         :return:
         """
@@ -170,9 +239,20 @@ class Job(object):
             )
         except AttributeError:
             pass
+        print("end transfer files ", time.now)
         self._transfer_time = time.now - start
 
     async def run(self, drone: "Drone"):
+        """
+        Handles the job's execution.
+        The job's runtime is given by max(calculation time, transfer time).
+        The calculation time is determined by `_calculate`, the transfer time by
+        `_transfer_inputfiles`.
+        The job will be executed successfully unless the selected drone does not
+        provide enough resources, is unavailable or an exception occurs.
+        :param drone: the drone object the job was allocated to and is executed in
+        :return:
+        """
         assert drone, "Jobs cannot run without a drone being assigned"
         self.drone = drone
         self.in_queue_until = time.now
@@ -181,6 +261,7 @@ class Job(object):
         try:
 
             start = time.now
+            print(start)
             async with Scope() as scope:
                 await instant
                 scope.do(self._transfer_inputfiles())
@@ -207,6 +288,13 @@ class Job(object):
 
 
 async def job_to_queue_scheduler(job_generator, job_queue):
+    """
+    Handles reading the simulation's job input and puts the job's into the job queue
+    :param job_generator: reader object that yields jobs from input
+    :param job_queue: queue the jobs are added to
+    :return:
+
+    """
     base_date = None
     for job in job_generator:
         if base_date is None:
