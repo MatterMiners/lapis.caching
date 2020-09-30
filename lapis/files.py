1f70388f20d619a2ee597b2990bca7cd3400a592
tfesenbecker <t.fehsenbecker@gmx.de>
2020-09-30 11:16:05 +0200

    Updated files module documentation

--- lapis/files.py
+++ lapis/files.py
@@ -2,6 +2,10 @@ from typing import Optional, NamedTuple
 
 
 class StoredFile(object):
+    """
+    Object representing stored files
+
+    """
 
     __slots__ = (
         "filename",
@@ -22,22 +26,51 @@ class StoredFile(object):
         numberofaccesses: Optional[int] = None,
         **filespecs,
     ):
+        """
+        Intialization of a stored file
+        :param filename: name of the file
+        :param filesize: size of the file
+        :param storedsize: size of the file that is actually stored, necessary if
+        less than the whole file is stored
+        :param cachedsince: time when the file was cached
+        :param lastaccessed: time when the file was accessed the last time
+        :param numberofaccesses: number of times the file was accessed
+        """
         self.filename = filename
+        """name of the file """
         self.filesize = filesize
+        """size of the file"""
         self.storedsize = storedsize or self.filesize
+        """size of the file that is actually stored"""
         self.cachedsince = cachedsince
+        """point in time when the file was cached"""
         self.lastaccessed = lastaccessed
+        """time when the file was accessed the last time"""
         self.numberofaccesses = numberofaccesses
+        """number of times the file was accessed"""
 
     def increment_accesses(self):
+        """
+        Increments number of accesses of a file
+        """
         self.numberofaccesses += 1
 
 
 class RequestedFile(NamedTuple):
+    """
+    Representation of a requested file
+    """
     filename: str
+    """name of the file"""
     filesize: Optional[int] = None
+    """size of the file"""
 
     def convert_to_stored_file_object(self, currenttime):
+        """
+        Converts a requested file into a stored file
+
+        :param currenttime: point in time when the conversion takes place
+        """
         print(self.filesize)
         return StoredFile(
             self.filename,
@@ -49,6 +82,14 @@ class RequestedFile(NamedTuple):
 
 
 class RequestedFile_HitrateBased(NamedTuple):
+    """
+    Represents a requested file in hitrate based caching.
+    The cachehitrate flag is somewhat messed up currently.
+    **Its use should be reworked when remodeling the connection module.**
+    """
     filename: str
+    """name of the requested file"""
     filesize: int
+    """size of the requested file"""
     cachehitrate: int
+    """flag whether the file is cached, 1 if it is cached, 0 if it is not cached"""
