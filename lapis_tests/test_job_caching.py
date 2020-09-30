e06a8d7d658b7840ad036adb4f8857b148fac0bd
tfesenbecker <t.fehsenbecker@gmx.de>
2020-09-30 08:18:54 +0200

    Added template for unit tests of the job class when supporting caching

---
+++ lapis_tests/test_job_caching.py
@@ -0,0 +1,16 @@
+import pytest
+from usim import Scope, time
+
+from lapis.drone import Drone
+from lapis.job import Job
+
+from lapis_tests import via_usim, DummyScheduler, DummyDrone
+from lapis.connection import Connection
+
+
+class TestJobCaching(object):
+    def test_calculation_time(self):
+        pass
+
+    def test_transfer_time(self):
+        pass
