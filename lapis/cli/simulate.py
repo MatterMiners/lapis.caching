c105b8f663ec444898857e379c0718e66e1211f9
tfesenbecker <t.fehsenbecker@gmx.de>
2020-10-02 20:36:39 +0200

    Changed simulate.py to use FileBasedHitrateStorage instead of deprecated HitrateStorage. Issue to avoid hardcoding the storage type was created: https://github.com/tfesenbecker/lapis/issues/10

--- lapis/cli/simulate.py
+++ lapis/cli/simulate.py
@@ -11,7 +11,7 @@ from lapis.job_io.htcondor import htcondor_job_reader
 from lapis.pool import StaticPool, Pool
 from lapis.pool_io.htcondor import htcondor_pool_reader
 from lapis.job_io.swf import swf_job_reader
-from lapis.storageelement import StorageElement, HitrateStorage
+from lapis.storageelement import StorageElement, HitrateStorage, FileBasedHitrateStorage
 from lapis.storage_io.storage import storage_reader
 
 from lapis.scheduler import CondorJobScheduler
@@ -112,9 +112,7 @@ def static(ctx, job_file, pool_file, storage_files, remote_throughput, cache_hit
             storage_input=storage_file,
             storage_content_input=storage_content_file,
             storage_reader=storage_import_mapper[storage_type],
-            storage_type=partial(HitrateStorage, cache_hitrate)
-            if cache_hitrate is not None
-            else StorageElement,
+            storage_type=FileBasedHitrateStorage,
         )
     for current_pool in pool_file:
         pool_file, pool_file_type = current_pool
